ab7ec5cae9cdfb5ba60cfbe9e94c68a6818e5b9d
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-10-16 15:49:51 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/mestresistemico/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mestresistemico/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.mestresistemico.workshopmongo.domain.Post;
 import com.mestresistemico.workshopmongo.domain.User;
 import com.mestresistemico.workshopmongo.dto.AuthorDTO;
+import com.mestresistemico.workshopmongo.dto.CommentDTO;
 import com.mestresistemico.workshopmongo.repositories.PostRepository;
 import com.mestresistemico.workshopmongo.repositories.UserRepository;
 
@@ -39,10 +40,20 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO(sdf.parse("21/03/2018"), "Boa viagem Mano!", new AuthorDTO(alex));		
+		CommentDTO c2 = new CommentDTO(sdf.parse("22/03/2018"), "Aproveite", new AuthorDTO(bob));		
+		CommentDTO c3 = new CommentDTO(sdf.parse("23/03/2018"), "Tenha um ótimo dia", new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+
 	}
 
 }
